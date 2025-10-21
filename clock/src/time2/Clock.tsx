0b06aa713d023911be9e5aeda7758ece727e3bcc
n <nataliaklucnikova9@gmail.com>
2025-10-21 15:52:28 +0300

    \

--- clock/src/time2/Clock.tsx
+++ clock/src/time2/Clock.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Clock } from "../time/Clock";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './Clock.scss';
 
 export const Clock_alive = () =>{
+    const format_time = (num:any) => String(num).padStart(2, '0');
     const[time, setTime] = useState({
         hours: 0,
         minutes: 0,
@@ -27,5 +31,28 @@ export const Clock_alive = () =>{
         return () => clearInterval(time_interval);
     }, []);
 
-   return <Clock time={time} />
+   /*return <Clock time={time} />*/
+
+   return(
+    <div className="time">
+        <div className="card">
+            <div className="time-block">
+                <div className="time-number">{format_time(time.hours)}</div>
+                <div className="time-label">Часы</div>
+            </div>
+            <p>:</p>
+            
+            
+            <div className="time-block">
+                <div className="time-number">{format_time(time.minutes)}</div>
+                <div className="time-label">Минуты</div>
+            </div>
+            <p>:</p>
+            <div className="time-block">
+                <div className="time-number">{format_time(time.seconds)}</div>
+                <div className="time-label">Секунды</div>
+            </div>
+        </div>
+    </div>
+   )
 }
