518b4a5050d90e4bec984208e7dc9f35e5734982
n <nataliaklucnikova9@gmail.com>
2025-10-21 11:38:10 +0300

    /

---
+++ clock/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Clock} from './time/Clock';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='title justify-content-center d-flex'>
+        <h1>Текущее время</h1>
+      </div>
+      <Clock time = {{ hours: 1, minutes: 20, seconds: 15 }}/>
+      <Clock time = {{ hours: 12, minutes: 3, seconds: 29 }}/>
+      <Clock time = {{ hours: 6, minutes: 26, seconds: 5 }}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ clock/src/time/Clock.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './Clock.scss';
+
+export const Clock = ({ time }: any) =>{
+    const{ hours, minutes, seconds} = time;
+    const format_time = (num:any) => String(num).padStart(2, '0');
+
+    return(
+        <div className="time d-flex justify-content-center align-items-center flex-colum">
+            <div className="clock d-flex justify-content-center align-items-center ">
+                {format_time(hours)}:{format_time(minutes)}:{format_time(seconds)}  
+            </div>
+    </div>
+    )
+}
+
